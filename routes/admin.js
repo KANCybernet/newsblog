bf862f3e621ffd6ea4daa64fc3872b7be0dae5a9
kancybernet <kelechukwuuroko@gmail.com>
2021-12-05 21:50:30 +0100

    new update

--- routes/admin.js
+++ routes/admin.js
@@ -109,7 +109,7 @@ router
   createPost.findOne({_id:req.query.id})
   .then((result)=> {
     console.log(result);  
-    res.render('admin/edit',{title: "Edit Blog Post"})
+    res.render('admin/edit',{title: "Edit Blog Posts"})
   })
   .then((err) => console.log(err))
      
